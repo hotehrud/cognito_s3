f3c614d3fcb91436f1b5c2a121e41e9b4290e24e
hotehrud <hotehrud@naver.com>
2019-11-04 23:55:49 +0900

    Clear, config/s3.js

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,9 +1,9 @@
 const obj = {
-  BUCKET_NAME: 'cognito.s3',
-  AWS_REGION: 'ap-northeast-2',
-  COGNITO_IDENTITY_POOL_ID: 'ap-northeast-2:5d3671e8-c0aa-4b26-a006-413f55420e25',
-  COGNITO_USER_POOL_ID: 'ap-northeast-2_ei2MOvCsi',
-  COGNITO_CLIENT_ID: '2pca22a7ob9q3t389krtk7ms0v'
+  BUCKET_NAME: '',
+  AWS_REGION: '',
+  COGNITO_IDENTITY_POOL_ID: '',
+  COGNITO_USER_POOL_ID: '',
+  COGNITO_CLIENT_ID: ''
 };
 
 export const {
